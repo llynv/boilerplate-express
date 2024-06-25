966a80d0b06cab6bb40f6bd28bf9f0fc4add6e7a
Linh Võ <linhvg19804@gmail.com>
2024-06-25 11:22:43 +0000

    done

--- myApp.js
+++ myApp.js
@@ -1,8 +1,24 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser')
+const path = require('path')
+const homePage = path.resolve(__dirname, 'views', 'index.html')
+
+app.use(bodyParser.urlencoded({
+        extended: false
+    }))
+
+app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.sendFile(homePage)
+})
+
+app.post('/name', (req, res) => {
+    let {first, last} = req.body 
+    res.json({
+        name: `${first} ${last}`
+    })
 })
 
 
@@ -25,13 +41,4 @@ app.get('/', (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
